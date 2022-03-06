e406853ca02e7d81f2ad3ff9b4774d7ab1d5f8fd
Ravi Bastola <ravi@Ravis-Mac-mini.local>
2022-03-06 17:55:15 +0545

    add movie view-model, model and test

---
+++ MVVMIOS/MovieListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MovieListViewController.swift
+//  MVVMIOS
+//
+//  Created by Ravi Bastola on 3/6/22.
+//
+
+import Foundation
+import UIKit
+
+class MovieListViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
